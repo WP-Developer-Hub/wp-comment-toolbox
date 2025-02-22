44776bce8d9a20803c0fb60b8c3171d49e3f85b9
DJABhipHop <djabhiphop-DJABHipHop@yahoo.com>
2025-02-22 00:32:43 -0500

    Bump Version number form 6.0.0 => 6.0.1
    Bump Version number form 6.0.0 => 6.0.1

--- wp-comment-toolbox.php
+++ wp-comment-toolbox.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Comment Toolbox
  * Description: A comprehensive toolset for enhancing WordPress comment forms. It reorders comment fields, customizes user role visibility, adds a dark/light mode toggle for the comment toolbar, supports link management (including disabling clickable links).
- * Version: 6.0.0
+ * Version: 6.0.1
  * Author: DJABHipHop
  * Requires PHP: 7.2
  * Requires at least: 6.0
